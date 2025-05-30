9462c519d4113fa6a964cc01c84ae5ea75ee7f46
mohamedi01 <106253210+mohamedi01@users.noreply.github.com>
2025-05-29 18:50:42 -0700

    Create run_vcs.sh

---
+++ Final-Project/fpga/run_vcs.sh
@@ -0,0 +1,19 @@
+source ./exports.sh
+
+# Quantize kernel
+sbt -Dtest.VCS=true "; testOnly spatial.tests.QuantizeKernelTest"
+
+# Power spectrum
+sbt -Dtest.VCS=true "; testOnly spatial.tests.PowerSpectrumTest"
+
+# Mel filterbank
+sbt -Dtest.VCS=true "; testOnly spatial.tests.MelFilterbankTest"
+
+# Log compression
+sbt -Dtest.VCS=true "; testOnly spatial.tests.LogCompressTest"
+
+# Whisper scaling
+sbt -Dtest.VCS=true "; testOnly spatial.tests.WhisperScaleTest"
+
+# STFT
+sbt -Dtest.VCS=true "; testOnly spatial.tests.STFTKernelTest"
